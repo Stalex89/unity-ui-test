556d3325f2d904ff121acdd41e331f277b980c8f
alex <alex@example.com>
2018-02-12 22:44:22 +0100

    add skill cooldown modify lvlup panel correct text

---
+++ Assets/Scripts/UI/SkillCoolDown.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Script for UI skill cooldown
+public class SkillCoolDown : MonoBehaviour 
+{
+	public string skillButtonAxisName;
+	public Image skillIcon;
+	public Image darkMask;
+	public Text cooldownText;
+
+	[HideInInspector]
+	public float cooldownTimeLeft;
+	[HideInInspector]
+	public float nextReadyTime;
+	public float cooldownDuration;
+	
+	public void Initialize()
+	{
+		SkillReady();
+	}
+
+	void Start()
+	{	
+		Initialize();
+	}
+
+
+	void Update()
+	{
+		bool cooldownComplete = (Time.time > nextReadyTime);
+		if(cooldownComplete)
+		{
+			SkillReady();
+			if(Input.GetButtonDown(skillButtonAxisName))
+			{
+				ButtonTriggered();
+			}
+		}
+		else SkillCooldown();
+	}
+
+	// Disable cooldown text and mask when skill is ready
+	private void SkillReady()
+	{
+		cooldownText.enabled = false;
+		darkMask.enabled = false;
+	}
+
+	private void SkillCooldown()
+	{
+		cooldownTimeLeft -= Time.deltaTime;
+		float roundedCooldown = Mathf.Round(cooldownTimeLeft);
+		cooldownText.text = roundedCooldown.ToString();
+		darkMask.fillAmount = (cooldownTimeLeft/cooldownDuration);
+	}
+
+	private void ButtonTriggered()
+	{
+		nextReadyTime = cooldownDuration + Time.time;
+		cooldownTimeLeft = cooldownDuration;
+		darkMask.enabled = true;
+		cooldownText.enabled = true;
+
+		// Call skill initialization
+	}
+
+}
+
+
+
